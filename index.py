be6c8eb1a3d1a88c6430b48b27c2e07e54388295
sart sar <amirsartipi13@gmail.com>
2021-04-09 12:01:12 +0430

    finish remove stop words

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import os
 from elasticsearch import Elasticsearch, helpers
 from time import time
 from nltk.corpus import stopwords
+from gensim.parsing.preprocessing import remove_stopwords
 
 
 def drop_coulmns(source_file, result_file):
@@ -19,20 +20,24 @@ def drop_coulmns(source_file, result_file):
     else:
         raise Exception('source_file : {} not exists or result_file : {} exists'.format(source_file, result_file))
 
-# must implement
 def delete_stop_words(source_file):
+    t1 = time()
+    index = 0
     if os.path.exists(source_file):
         with open(source_file,"rt") as source:
             csv.field_size_limit(100000000)
             rdr= csv.reader(source)
-            with open('test_result.csv',"wt") as result:
+            with open('books_final.csv',"wt") as result:
                 wtr= csv.writer(result)
                 for r in rdr:
+                    index+=1
+                    print(index)
                     if len(r)==4:
-                        word_list = r[3].split()
-                        filtered_words = [word for word in word_list if word not in stopwords.words('english')]
-                        r[3] = ' '.join(filtered_words)
+                        r[3] = r[3].lower()
+                        r[3] = remove_stopwords(r[3])
                         wtr.writerow(r) 
+    print("done delete stop words in  {} s".format(time()-t1))
+
 
 def delete_existing_index(index):
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
@@ -57,7 +62,7 @@ if __name__ == '__main__':
     # drop_coulmns('en-books-dataset.csv', 'books.csv')
     delete_existing_index('books')
     # delete_stop_words('books.csv')
-    csv_reader_index('books', 'books.csv')
+    csv_reader_index('books', 'books_final.csv')
     print('done !')
 
     
